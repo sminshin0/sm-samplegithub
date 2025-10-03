d8cf8c99bac3bdfbb95feba5cc7446d163481448
smin <sminshin0@gmail.com>
2025-10-03 18:55:29 +0900

    feat: clean project

--- go.mod
+++
@@ -1,5 +0,0 @@
-module hello-world
-
-go 1.21
-
-// 의존성이 없는 간단한 프로젝트
--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-0ss4kx0a"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "my-terraform-project-terraform-state-lock"
-    encrypt        = true
-  }
-}
